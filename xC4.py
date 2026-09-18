7cf1449b9d26752072dc2ce8a80b565fbd442e86
Subhajit Maji <90397177+subhajit-maji@users.noreply.github.com>
2026-09-18 21:11:51 +0530

    Update xC4.py

--- xC4.py
+++ xC4.py
@@ -1,4 +1,4 @@
-# By AbdeeLkarim BesTo
+# By SM
 
 import requests , json , binascii , time , urllib3 , base64 , datetime , re ,socket , threading , random , os , asyncio
 from protobuf_decoder.protobuf_decoder import Parser
